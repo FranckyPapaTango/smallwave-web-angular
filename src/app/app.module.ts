b6da3e8bc149a64f75360e2be196cfa7466393dc
rafar <rafaralahyf@yahoo.fr>
2022-10-16 06:12:37 +0200

    pure typescript angular navbar component - 16/10/2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,13 +21,21 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { PartenairesComponent } from './components/partenaires/partenaires.component';
 import { ServdevComponent } from './components/servdev/servdev.component';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav' ;
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { NavbComponent } from './components/navb/navb.component' ;
-import { MatListModule } from '@angular/material/list' ;
+
+
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
@@ -55,14 +63,18 @@ import { RouterModule, Routes } from '@angular/router';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-
     NgImageSliderModule,
+
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatTabsModule,
     MatSidenavModule,
+    MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatToolbarModule,
-    FlexLayoutModule,
     MatListModule,
+    MatMenuModule,
 
     FormsModule,
     RouterModule
--- src/app/components/navb/navb.component.ts
+++ src/app/components/navb/navb.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav' ;
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list' ;
-import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-navb',
@@ -15,17 +9,28 @@ import { Output, EventEmitter } from '@angular/core';
 })
 export class NavbComponent implements OnInit {
 
- @Output() sidenavClose = new EventEmitter();
+authStatus: boolean =false;
+menuBooleanVariable : boolean =false;
+menu_icon_class_BooleanVariable: boolean =false;
 
-  constructor() { }
+constructor() {}
 
-  ngOnInit(): void {
+ ngOnInit(): void {
 
   }
 
 
-  public onSidenavClose = () => {
-    this.sidenavClose.emit();
-  }
+
+
+openMenu(){
+/* this.menuBooleanVariable=true;  to toggle : */
+this.menuBooleanVariable= ! this.menuBooleanVariable;
+/* toggled too */
+this.menu_icon_class_BooleanVariable= ! this.menu_icon_class_BooleanVariable;
+}
+
+
+
+
 
 }
